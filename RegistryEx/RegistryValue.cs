8f354108827a863b9159ff56d4e44c738b247755
Kaciras <Kaciras@protonmail.com>
2022-07-13 15:06:08 +0800

    Fix Equals & GetHashCode for RegistryValue

--- RegistryEx/RegistryValue.cs
+++ RegistryEx/RegistryValue.cs
@@ -1,10 +1,68 @@
-﻿using Microsoft.Win32;
+﻿using System;
+using System.Linq;
+using Microsoft.Win32;
 
 namespace RegistryEx;
 
-public record struct RegistryValue(
-	object Value,
-	RegistryValueKind Kind)
+public readonly struct RegistryValue : IEquatable<RegistryValue>
 {
+	public static readonly RegistryValue DELETED = default;
+
 	public bool IsDelete => Kind == RegistryValueKind.Unknown;
+
+	public readonly object Value { get; }
+
+	public readonly RegistryValueKind Kind { get; }
+
+	public RegistryValue(object value, RegistryValueKind kind)
+	{
+		Value = value;
+		Kind = kind;
+	}
+
+	public override bool Equals(object obj)
+	{
+		return obj is RegistryValue v && Equals(v);
+	}
+
+	public bool Equals(RegistryValue other)
+	{
+		return Kind == other.Kind && Kind switch
+		{
+			RegistryValueKind.Unknown => true,
+			RegistryValueKind.MultiString => ArrayEquals<string>(other.Value),
+			RegistryValueKind.Binary or
+			RegistryValueKind.None => ArrayEquals<byte>(other.Value),
+			_ => Value.Equals(other.Value),
+		};
+	}
+
+	bool ArrayEquals<T>(object otherValue)
+	{
+		if (otherValue is not T[] || otherValue == null)
+		{
+			return false;
+		}
+		return ((T[])Value).SequenceEqual((T[])otherValue);
+	}
+
+	public override int GetHashCode() => (int)Kind ^ Kind switch
+	{
+		RegistryValueKind.Unknown => 0,
+		RegistryValueKind.MultiString => ArrayHashCode((string[])Value),
+		RegistryValueKind.Binary or
+		RegistryValueKind.None => ArrayHashCode((byte[])Value),
+		_ => Value.GetHashCode(),
+	};
+
+	int ArrayHashCode<T>(T[] values)
+	{
+		return values.Aggregate(values.Length, (s, v) => unchecked(s * 31 + v!.GetHashCode()));
+	}
+
+	public static RegistryValue From(RegistryKey key, string name)
+	{
+		var value = key.GetValue(name, null, RegistryValueOptions.DoNotExpandEnvironmentNames);
+		return value == null ? DELETED : new(value, key.GetValueKind(name));
+	}
 }
---
+++ Test/RegistryValueTest.cs
@@ -0,0 +1,65 @@
+﻿namespace RegistryEx.Test;
+
+[TestClass]
+public sealed class RegistryValueTest
+{
+	readonly RegistryValue[] values = new RegistryValue[]
+	{
+		RegistryValue.DELETED,
+		new(Array.Empty<byte>(), RegistryValueKind.None),
+		new(Array.Empty<byte>(), RegistryValueKind.Binary),
+		new(new byte[1], RegistryValueKind.Binary),
+		new(new byte[]{ 1 }, RegistryValueKind.Binary),
+		new(0L, RegistryValueKind.QWord),
+		new(1L, RegistryValueKind.QWord),
+		new(0, RegistryValueKind.DWord),
+		new("", RegistryValueKind.ExpandString),
+		new("", RegistryValueKind.String),
+		new(Array.Empty<string>(), RegistryValueKind.MultiString),
+	};
+
+	public static IEnumerable<object[]> DifferentValues()
+	{
+		var values = new RegistryValueTest().values;
+		var length = values.Length;
+
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < length; j++)
+			{
+				if (i != j)
+				{
+					yield return new object[] { values[i], values[j] };
+				}
+			}
+		}
+	}
+
+	public static IEnumerable<object[]> EqualValues()
+	{
+		var values0 = new RegistryValueTest().values;
+		var values1 = new RegistryValueTest().values;
+		var length = values0.Length;
+
+		for (int i = 0; i < length; i++)
+		{
+			yield return new object[] { values0[i], values1[i] };
+		}
+	}
+
+	[DynamicData(nameof(EqualValues), DynamicDataSourceType.Method)]
+	[DataTestMethod]
+	public void Equality0(RegistryValue a, RegistryValue b)
+	{
+		Assert.AreEqual(a, b);
+		Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+	}
+
+	[DynamicData(nameof(DifferentValues), DynamicDataSourceType.Method)]
+	[DataTestMethod]
+	public void Equality1(RegistryValue a, RegistryValue b)
+	{
+		Assert.AreNotEqual(a, b);
+		Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+	}
+}
