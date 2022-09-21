4105ae187815a19f32f11a5834940aab8378aab5
Kaciras <Kaciras@protonmail.com>
2022-09-21 20:55:09 +0800

    updated deps

--- Test/TestFixture.cs
+++ Test/TestFixture.cs
@@ -1,4 +1,8 @@
-﻿namespace RegistryEx.Test;
+﻿using System.Security.Principal;
+using System.Linq;
+using System.Security.AccessControl;
+
+namespace RegistryEx.Test;
 
 internal readonly struct TestFixture : IDisposable
 {
@@ -12,5 +16,17 @@ internal readonly struct TestFixture : IDisposable
 	public void Dispose()
 	{
 		Registry.CurrentUser.DeleteSubKeyTree("_RH_Test_", false);
+	}
+
+	public static void RemoveCurrentUserACL(RegistryKey key)
+	{
+		var user = WindowsIdentity.GetCurrent().User!;
+		var accessControl = key.GetAccessControl();
+
+		var rule = new RegistryAccessRule(user, RegistryRights.FullControl, AccessControlType.Deny);
+		accessControl.SetAccessRule(rule);
+
+		accessControl.SetAccessRuleProtection(true, true);
+		key.SetAccessControl(accessControl);
 	}
 }
